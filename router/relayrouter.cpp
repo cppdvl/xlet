1cd4c48c56c227613b06bf66645f05f17cdc7c70
cppdvl <cpp.dvl@gmail.com>
2024-01-15 19:10:05 -0500

    router example enhanced

--- router/relayrouter.cpp
+++ router/relayrouter.cpp
@@ -3,18 +3,13 @@
 //
 #include "xlet.h"
 #include <set>
-#include <queue>
 #include <iomanip>
 #include <sstream>
 
 
-std::set<uint64_t> connectedClients{};
-
-//Top of the queue is the host.
-std::queue<uint64_t> orderToHost{};
-
-
-
+std::set<uint64_t> authorizedPeers{};
+std::set<uint64_t> nonHostingPeers{};
+static uint64_t sHostId = 0;
 
 void log(std::string letId, const std::string& msg)
 {
@@ -86,42 +81,88 @@ void handleServerIncomingData(uint64_t peerId, std::vector<std::byte>& data) {
 
 void newConnection(uint64_t peerId)
 {
-    log(xlet::UDPlet::letIdToIpString(peerId), "New connection");
+    //Rule 1. if peerId is not in the group of authorized peers, then reject the connection.
+    auto ipString = xlet::UDPlet::letIdToIpString(peerId);
+    if (!authorizedPeers.contains(peerId))
+    {
+        log(ipString, "newConnection: Unauthorized connection");
+        return;
+    }
 
-    if (!connectedClients.empty() || !orderToHost.empty()) connectedClients.insert(peerId);
-    orderToHost.push(peerId);
+    //Rule 2. if peerId is already connected, then ignore the connection.
+    if (nonHostingPeers.contains(peerId) || sHostId == peerId)
+    {
+        log(ipString, "newConnection: Already connected");
+        if (sHostId == peerId) log(ipString, "newConnection: attempted Peer is the already host");
+        return;
+    }
+
+    //Rule 3. If hostId is not connected
+    log(ipString, "newConnection: New connection");
+    if (sHostId == 0)
+    {
+        sHostId = peerId;
+        log(ipString, "newConnection: New host");
+    }
+    else
+    {
+        log(ipString, "newConnection: New client");
+        nonHostingPeers.insert(peerId);
+    }
+}
+
+void purgeConnections()
+{
+    log("-- x --", "purgeConnections: Purging connections");
+    sHostId = 0;
+    nonHostingPeers.clear();
+    authorizedPeers.clear();
+}
+
+void removeConnection(uint64_t peerId)
+{
+    if (sHostId == peerId) sHostId = 0;
+    else if (nonHostingPeers.contains(peerId)) nonHostingPeers.erase(peerId);
+    else {
+        log(xlet::UDPlet::letIdToIpString(peerId), "removeConnection: Peer not found");
+        return;
+    }
+
+    log(xlet::UDPlet::letIdToIpString(peerId), "removeConnection: Peer removed");
+    log(xlet::UDPlet::letIdToIpString(peerId), "removeConnection: Host removed");
 }
 
 
 void runServerRouting(const std::string& ip, int port)
 {
-    xlet::UDPInOut udpServer(ip, 8899, true);
+    xlet::UDPInOut udpServer(ip, port, true);
 
     udpServer.letBindedOn.Connect(+[](uint64_t letId, std::__thread_id id){
         std::stringstream ss; ss << "UDP Server binded and waiting for data on thread: " << id;
         log(xlet::UDPlet::letIdToString(letId) , ss.str());
     });
+
     udpServer.enableQueueManagement();
+
     auto qThread = std::function<void()>([&udpServer](){
         while(true) {
 
             if (udpServer.qin_.size() > 0) {
 
-                //If Check peer is in connectedClients.
+                //If Check peer is in nonHostingPeers.
                 auto peerIdData = udpServer.qin_.front();
                 udpServer.qin_.pop();
 
                 //Get peerId
                 auto peerId = peerIdData.first;
 
-                //Check if it's a new connection. If so, add it to the connectedClients set.
-                if (connectedClients.find(peerId) == connectedClients.end()) newConnection(peerId);
-                auto hostId = orderToHost.front();
+                //Check if it's a new connection. If so, add it to the nonHostingPeers set.
+                if (nonHostingPeers.find(peerId) == nonHostingPeers.end()) newConnection(peerId);
 
                 //Broadcast to all connected clients
-                if (hostId == peerId) for (auto& client : connectedClients)  udpServer.pushData(client, peerIdData.second);
+                if (sHostId == peerId) for (auto& client : nonHostingPeers)  udpServer.pushData(client, peerIdData.second);
                 //Route data to host.
-                else udpServer.pushData(hostId, peerIdData.second);
+                else udpServer.pushData(sHostId, peerIdData.second);
             }
         }
     });
@@ -141,8 +182,16 @@ void runServerRouting(const std::string& ip, int port)
 
 int main(int argc, char**argv)
 {
+    //Argv[1] is the port
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+        return 1;
+    }
 
-    runServerRouting("127.0.0.1", 8899);
+    int port = std::stoi(argv[1]);
+    runServerRouting("0.0.0.0", port);
+
     return 0;
 
 }
